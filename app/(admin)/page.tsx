a1d50e39de973145069d9ceef22f8a9ebda1f6b0
Dell <muhammadsarmad2003@email.com>
2025-07-01 12:16:15 -0700

    Initial commit

---
+++ app/(admin)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1>Lets build Assistly AI Agent App</h1>
+    </main>
+  );
+}
